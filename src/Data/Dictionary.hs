37364e0bf3f9a7d4bcf9dc7192f49ecd23e8b13f
Peter Robinson <16293045+pwrobinson@users.noreply.github.com>
2019-10-23 13:53:30 +0800

    changed class to IntDictionary

--- src/Data/Dictionary.hs
+++ src/Data/Dictionary.hs
@@ -5,5 +5,5 @@ import Data.Dictionary.Request
 
 
 -- | A Dictionary data structure `d` that is able to run requests of type `Request k` in monad `m`. Return values signals whether the request was successful.
-class Dictionary d k m where
-    runRequest :: Monad m => Request k -> d -> m Bool
+class IntDictionary d m where
+    runRequest :: Monad m => Request Int -> d -> m Bool
